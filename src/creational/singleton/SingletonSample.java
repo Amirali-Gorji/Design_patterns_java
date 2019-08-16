0a0275aa512fb052516dc3f0d4abd455abdb5a61
hamid <nazerpanahi@gmail.com>
2019-08-16 12:10:54 +0430

    a sample of Singleton model

---
+++ src/creational/singleton/SingletonSample.java
@@ -0,0 +1,28 @@
+package creational.singleton;
+
+/**
+ * @author Nazerpanahi
+ * This class is a sample of the singleton model.
+ * It is not thread-safe.
+ * It has only one constructor and also only on method.
+ * The constructor is private.
+ * Because of the lazy loading the single object instantiate in the getInstance method.
+ */
+public class SingletonSample {
+    // single object
+    private Object o=null;
+
+    private SingletonSample(){
+    }
+
+    /**
+     * @return the single object
+     */
+    public Object getInstance(){
+        // because of the lazy loading the o instantiate here when this method call by user.
+        if (o == null){
+            o=new Object();
+        }
+        return o;
+    }
+}
